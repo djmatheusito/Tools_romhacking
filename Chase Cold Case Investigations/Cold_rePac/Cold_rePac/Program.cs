2e5db5b2cfdd48a502eb2378248963794f036349
djmatheusito <30787327+djmatheusito@users.noreply.github.com>
2018-12-23 21:22:42 -0200

    Add files via upload

---
+++ Chase Cold Case Investigations/Cold_rePac/Cold_rePac/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cold_rePac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------Cold_rePACker v1.0.0-----------");
+            Console.WriteLine("(EN)");
+            Console.WriteLine("Put the original .pac file in the same program folder.");
+            Console.WriteLine("Enter the name of the extracted .pac folder:\n");
+            Console.WriteLine("(BR)");
+            Console.WriteLine("Coloque o arquivo .pac original na mesma pasta do programa.");
+            Console.WriteLine("Entre com o nome da pasta do .pac extraido:\n");
+            string nomePasta = Console.ReadLine();
+            string[] pasta = Directory.GetFiles(nomePasta);
+            int tamanhoNome = 64;
+            int posTabela = 60;
+
+            Console.WriteLine("Inserindo...");
+            Console.WriteLine("Inserting...\n");
+
+            foreach (string arquivo in pasta)
+            {
+                // NovoPac(arquivo, nomeNomePasta);
+
+                
+
+                using (BinaryReader b = new BinaryReader(File.Open(nomePasta + ".pac", FileMode.Open)))
+                {
+                    
+                    b.BaseStream.Seek(posTabela, SeekOrigin.Begin);
+                    int offsetArq = b.ReadInt32();
+                    byte[] nome = b.ReadBytes(tamanhoNome);
+                    string nomeAsc = Encoding.ASCII.GetString(nome).TrimEnd('\0');
+                    byte[] arquivos = File.ReadAllBytes(nomePasta +@"\\"+ nomeAsc);
+                    b.Close();
+
+
+                    using (BinaryWriter w = new BinaryWriter(File.Open(nomePasta + ".pac", FileMode.Open)))
+                    {
+                        w.BaseStream.Seek(offsetArq, SeekOrigin.Begin);
+                        w.Write(arquivos);
+                        w.Close();
+
+                    }
+
+                    posTabela += 144;
+
+                }
+            }
+
+            Console.WriteLine("\nArquivos inseridos!");
+            Console.WriteLine("Files inserted!");
+        }
+
+        
+    }
+}
---
+++ Chase Cold Case Investigations/Cold_unPAC/Cold_unPAC/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cold_unPAC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int posNumeroArqs = 12;
+            Console.WriteLine("-----------Cold_unPACker v1.0.0-----------");
+            Console.WriteLine("(EN)");
+            Console.WriteLine("Enter with .pac file name(without extension):\n");
+            Console.WriteLine("(BR)");
+            Console.WriteLine("Entre com o nome do arquivo .pac(sem a extensão):\n");
+            
+         //   string nomeArq = Console.ReadLine();
+            Console.WriteLine("Extraindo...");
+            Console.WriteLine("Extracting...");
+            string[] pacs = Directory.GetFiles("pacs");
+
+            foreach (string pac in pacs)
+            {
+                int numeroArqs;
+                int posTabela = 56;
+                int tamanhoNome = 64;
+
+                using (BinaryReader leitor = new BinaryReader(File.Open(pac, FileMode.Open)))
+                {
+                    leitor.BaseStream.Seek(posNumeroArqs, SeekOrigin.Begin);
+                    numeroArqs = leitor.ReadInt32();
+                    string[] diretorio = pac.Split('.');
+                    Directory.CreateDirectory(diretorio[0]);
+
+                    for (int i = 0; i < numeroArqs; i++)
+                    {
+
+                        leitor.BaseStream.Seek(posTabela, SeekOrigin.Begin);
+                        int tamanhoArq = leitor.ReadInt32();
+                        int ponteiroArq = leitor.ReadInt32();
+                        byte[] nome = leitor.ReadBytes(tamanhoNome);
+                        string nomeAsc = Encoding.ASCII.GetString(nome).TrimEnd('\0');
+
+                        leitor.BaseStream.Seek(ponteiroArq, SeekOrigin.Begin);
+                        byte[] arquivo = leitor.ReadBytes(tamanhoArq);
+                        File.WriteAllBytes(diretorio[0] + @"\" + nomeAsc, (arquivo));
+
+                        nomeAsc = null;
+                        posTabela += 144;
+                    }
+
+                    leitor.Close();
+
+                    Console.WriteLine("\nExtração completa!");
+                    Console.WriteLine("Extraction completed!");
+
+                }
+            }
+
+           
+
+
+        }
+    }
+}
