55dcbb94dd0c4770d2db1a8e588de39050abe39c
djmatheusito <30787327+djmatheusito@users.noreply.github.com>
2018-12-23 21:16:02 -0200

    Add files via upload

---
+++ Metroid Samus Returns/Metroid_SR_Btxt_Creator/Metroid_SR_Btxt_Creator/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Metroid_SR_Btxt_Creator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do arquivo .txt(sem a extensão) que foi dumpado e pressione enter para iniciar.");
+            string nomeArq = Console.ReadLine();
+            string nomeArqCerto = nomeArq.ToLower();
+            Console.WriteLine("Criando novo txt...");
+            using (StreamReader sr = new StreamReader(nomeArqCerto + ".txt"))
+            {
+
+                string texto = sr.ReadToEnd().Replace("\n", string.Empty)
+                                             .Replace("\r", string.Empty)
+                                             .Replace("ã", "ä")
+                                             .Replace("õ", "ö")
+                                             .Replace("Ã", "Ä")
+                                             .Replace("Õ", "Ö")
+                                             .Replace("(A)", "☯")
+                                             .Replace("(R)", "⚭")
+                                             .Replace("(Y)", "☵")
+                                             .Replace("(L)", "☮")
+                                             .Replace("(Circle Pad)", "☷")
+                                             .Replace("(X)", "☶")
+                                             .Replace("(UP)", "☱")
+                                             .Replace("(RIGHT)", "☲")
+                                             .Replace("(DOWN)", "☳")
+                                             .Replace("(LEFT)", "☴")
+                                             .Replace("(B)", "☰");
+
+                string[] splitString = texto.Split('<', '>');
+                sr.Close();
+
+                byte[] separador = new byte[] { 0x00 };
+                byte[] header = new byte[] { 0x42, 0x54, 0x58, 0x54, 0x01, 0x00, 0x08, 0x00 };
+
+                if (File.Exists("Novo_" + nomeArqCerto + ".txt"))
+                {
+                    File.Delete("Novo_" + nomeArqCerto + ".txt");
+
+                }
+
+                File.Create("Novo_" + nomeArqCerto + ".txt").Close();
+                using (BinaryWriter w2 = new BinaryWriter(File.Open("Novo_" + nomeArqCerto + ".txt", FileMode.Open)))
+                {
+                    w2.Write(header);
+                    w2.Close();
+                }
+
+                int inde = 8;
+
+
+                for (int i = 1; i < 985;)
+                {
+
+                    byte[] asc = Encoding.ASCII.GetBytes(splitString[i++]);
+                    byte[] unicode = Encoding.Unicode.GetBytes(splitString[i++].Replace("[Q]", "|"));
+
+                    using (BinaryWriter w = new BinaryWriter(File.Open("Novo_" + nomeArqCerto + ".txt", FileMode.Open)))
+                    {
+                        w.BaseStream.Seek(inde, SeekOrigin.Begin);
+                        w.Write(asc);
+                        w.Write(separador);
+                        w.Write(unicode);
+                        w.Write(separador);
+                        w.Write(separador);
+
+
+                        long tamanhoAsc = asc.Length + 1;
+                        int tamanhoStAsc = ((int)tamanhoAsc);
+
+                        long tamanhoUni = unicode.Length + 2;
+                        int tamanhoStUni = ((int)tamanhoUni);
+
+                        inde += tamanhoStAsc;
+                        inde += tamanhoStUni;
+
+                        w.Close();
+                    }
+
+
+
+                }
+
+                Console.WriteLine("Novo .txt criado, pressione qualquer tecla para sair.");
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
---
+++ Metroid Samus Returns/Metroid_SR_Dumper/Metroid_SR_Dumper/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Metroid_SR_Dumper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dumper de textos - Metroid Samus Returns 3DS");
+            Console.WriteLine("Digite o nome do arquivo .txt(btxt) (sem a entensão) que deseja extrair e pressione enter para inicar a extração");
+            string nomeArquivo = Console.ReadLine();
+            string nomeCerto = nomeArquivo.ToLower();
+            int posicaoTexto = 8;
+            long t = new FileInfo(nomeCerto + ".txt").Length - 8;
+            int tamanhoDoarquivo = ((int)t);
+            List<string> dialogos = new List<string>();
+
+            Console.WriteLine("Extraindo...");
+            while (posicaoTexto < tamanhoDoarquivo)
+                {
+
+                    using (var b = new BinaryReader(File.OpenRead(nomeCerto + ".txt")))
+                    {
+                        b.BaseStream.Seek(posicaoTexto, SeekOrigin.Begin);
+
+                        string StringAsc = "";
+                        char ch;
+                        while ((ch = b.ReadChar()) != 0)
+                        StringAsc = StringAsc + ch;
+
+                        dialogos.Add("<"+StringAsc+">");
+
+                        int separador = 1;
+                        long tamanhoAsc = StringAsc.Length + separador;
+                        int tamanhoStAsc = ((int)tamanhoAsc);
+
+                        posicaoTexto += tamanhoStAsc;
+
+                        b.Close();
+
+                            using (StreamReader sr = new StreamReader(nomeCerto + ".txt", Encoding.Unicode))
+                            {
+                                sr.BaseStream.Seek(posicaoTexto, SeekOrigin.Begin);
+
+                                string uni = sr.ReadToEnd();
+                                string[] sep = uni.Split('\0');
+                                dialogos.Add(sep[0].Replace("☯", "(A)")
+                                                   .Replace("⚭", "(R)")
+                                                   .Replace("☵", "(Y)")
+                                                   .Replace("☮", "(L)")
+                                                   .Replace("☷", "(Circle Pad)")
+                                                   .Replace("☶", "(X)")
+                                                   .Replace("☱", "(UP)")
+                                                   .Replace("☲", "(RIGHT)")
+                                                   .Replace("☳", "(DOWN)")
+                                                   .Replace("☴", "(LEFT)")
+                                                   .Replace("☰", "(B)")
+                                                   .Replace("|", "[Q]\n") + "\n");
+
+                                long tamanhoUniC = 2 + (sep[0].Length *2);
+                                int tamanhoStUniC = ((int)tamanhoUniC);
+                                posicaoTexto += tamanhoStUniC;
+                                sr.Close();
+                            }
+
+                                StringAsc = string.Empty;
+                    }
+
+                
+            }
+                        File.WriteAllLines(nomeCerto + "D" + ".txt", dialogos);
+                        Console.WriteLine("Extração completa, pressione qualquer tecla para sair.");
+                        Console.ReadLine();
+        }
+    }
+}
